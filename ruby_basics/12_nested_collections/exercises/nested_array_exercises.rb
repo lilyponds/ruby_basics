cbd892858a29370b2d0fa473b47839390a2afa98
tg <157854871+lilyponds@users.noreply.github.com>
2024-05-14 23:23:52 -0500

    Complete TOP Ruby Basics Excercies - Pending revision of the module and code comments.

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -1,8 +1,10 @@
 def blank_seating_chart(number_of_rows, seats_per_row)
+  seating_chart = Array.new(number_of_rows) {Array.new(seats_per_row)}
+  seating_chart
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
   # represent that each seat is empty.
-
+  
   # Example: blank_seating_chart(2, 3) should return:
   # [
   #   [nil, nil, nil],
@@ -14,45 +16,58 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
+  chart[row_index].push(seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
-
+  chart[row_index].delete_at(seat_index)
+  chart
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  chart
 end
 
 def count_empty_seats(chart)
+  counter = 0
+  chart.each do |row| 
+    row.each do |item|
+      if item==nil
+        counter += 1
+      end
+    end
+  end
+  counter
   # take a chart and return the number of empty (nil) seats in it
-
+  
   # NOTE: `chart` should **not** be mutated
 end
+  
 
-def find_favorite(array_of_hash_objects)
+
   # take an array_of_hash_objects and return the hash which has the key/value
   # pair :is_my_favorite? => true. If no hash returns the value true to the key
   # :is_my_favorite? it should return nil
 
   # array_of_hash_objects will look something like this:
-  # [
-  #   { name: 'Ruby', is_my_favorite?: true },
-  #   { name: 'JavaScript', is_my_favorite?: false },
-  #   { name: 'HTML', is_my_favorite?: false }
-  # ]
-
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
-end
+  def find_favorite(array_of_hash_objects)
+    array_of_hash_objects.find { |hash_object| hash_object[:is_my_favorite?] == true}
+  end
--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,25 +1,37 @@
 def nil_array(number)
-  # return an array containing `nil` the given number of times
+  arr = []
+  number.to_i.times {|i| arr.push(nil)}
+  arr
+    # return an array containing `nil` the given number of times
 end
 
 def first_element(array)
+  array[0]
   # return the first element of the array
 end
 
 def third_element(array)
+  array[2]
   # return the third element of the array
 end
 
 def last_three_elements(array)
+  if array.length >= 3 
+    array[-3..-1]
+  else 
+    array
+  end
   # return the last 3 elements of the array
 end
 
 def add_element(array)
+  array.push(23)
   # add an element (of any value) to the array
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
+  array.pop
 
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
@@ -27,35 +39,44 @@ end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
+  if array.length >=3
+    3.times {array.shift}
+    else
+      array = []
+    end
 
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
 
 def array_concatenation(original, additional)
-  # return an array adding the original and additional array together
+  original.concat(additional)# return an array adding the original and additional array together
 end
 
 def array_difference(original, comparison)
-  # return an array of elements from the original array that are not in the comparison array
+  original - comparison # return an array of elements from the original array that are not in the comparison array
 end
 
 def empty_array?(array)
-  # return true if the array is empty
+  array.empty?
 end
 
 def reverse(array)
+  array.reverse
   # return the reverse of the array
 end
 
 def array_length(array)
+  array.length
   # return the length of the array
 end
 
 def include?(array, value)
+  array.include?(value)
   # return true if the array includes the value
 end
 
 def join(array, separator)
+  array.join("#{separator}")
   # return the result of joining the array with the separator
 end
